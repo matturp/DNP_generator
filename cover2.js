a13f3afc51dd4a0d258836a4fd2b10d7d06a1d1f
matturp <81508090+matturp@users.noreply.github.com>
2021-05-11 18:01:14 +0100

    Add files via upload

---
+++ cover2.js
@@ -0,0 +1,46 @@
+let hiArray;
+let pg;
+let roboto;
+let slider;
+
+function setup() {
+  createCanvas(1638, 1077);
+
+  slider = createSlider(0, 90, 20, 1);
+  slider.position(10, 0);
+  slider.style('width', '80px');
+}
+
+function draw() {
+  background(255);
+  var wave = sin(radians(frameCount)) * 100;
+
+  for (var a = 0; a < width-100; a = a + 275){
+    for (var b = 0; b < height-100; b = b  + 20){
+
+      let val = slider.value();
+
+      fill(0);
+      textSize(20);
+      translate(mouseX,mouseY);
+      rotate(val);
+
+      text('Designers',50+a,50+b);
+      rotate(val);
+      text('Networks',150+a,50+b);
+      rotate(val);
+      text('Positions',240+a,50+b);
+    }
+  }
+
+} 
+
+
+
+
+function keyPressed(){
+
+  if (keyCode === ENTER) {
+    save('cover.jpg');
+  }
+}
