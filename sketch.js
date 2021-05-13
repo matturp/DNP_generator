eae9b2753d50c7390232318d599cd1d1e267aef7
matturp <81508090+matturp@users.noreply.github.com>
2021-05-13 14:55:54 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let slider;
 let slider2;
+let slider3;
 
 function setup() {
   createCanvas(3274, 2514);
@@ -11,6 +12,10 @@ function setup() {
   slider2 = createSlider(0, 50, 25, 5);
   slider2.position(300, height+50);
   slider2.style('width', '200px');
+  
+  slider3 = createSlider(0, 5000, 50, 25);
+  slider3.position(500, height+50);
+  slider3.style('width', '500px');
 }
 
 function draw() {
@@ -21,17 +26,18 @@ function draw() {
 
       let val = slider.value();
       let txtsize = slider2.value();
+      let space = slider3.value();
 
       fill(0);
       textSize(txtsize);
       translate(mouseX,mouseY);
       rotate(val);
       //text('Networks•Positions•Designers•',a,b);
-      text('Designers',50+a,50+b);
+      text('Designers',space+a,space+b);
       rotate(val);
-      text('Networks',150+a,50+b);
+      text('Networks',space+a,space+b);
       rotate(val);
-      text('Positions',240+a,50+b);
+      text('Positions',space+a,space+b);
     }
   }
 
